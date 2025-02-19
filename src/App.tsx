422cbb37027498bddc8ff3e38aa9ca7cccd7d6aa
Gothsec <oscarandreshernandezpineda@gmail.com>
2025-02-19 13:03:24 -0500

    Tailwindcss config

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import './App.css'
 
 function App() {
 
   return (
     <>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hotel management app</h1>
+      <p className="text-xl">This is a simple hotel management app</p>
+    </div>
     </>
   )
 }
